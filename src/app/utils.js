a18d61a8225f78bb8dda2076cc91bb5ce50dd1a7
rokaux <rokaux@gmail.com>
2016-04-23 16:22:24 +0300

    Еще больше модулей
    Правки согласно замечаний наставника

--- src/app/utils.js
+++ src/app/utils.js
@@ -2,52 +2,59 @@
 
 'use strict';
 
+/**
+ * Показываем элемент
+ */
+function showElement(element) {
+  element.classList.remove('invisible');
+}
+
+/**
+ * Скрываем элемент
+ */
+function hideElement(element) {
+  element.classList.add('invisible');
+}
+
+/**
+ * Метод Троттлинг (пропуск кадров)
+ */
+function throttle(callback, limit) {
+  var wait = false;
+  return function() {
+    if (!wait) {
+      callback.call();
+      wait = true;
+      setTimeout(function() {
+        wait = false;
+      }, limit);
+    }
+  };
+}
+
+/**
+ * Метод, который проверяет виден ли блок на экране
+ */
+function isElementVisible(target) {
+  var targetPosition = target.getBoundingClientRect();
+  return targetPosition.bottom > 0;
+}
+
+/**
+ * Смещение элемента на скролл сртаницы. Параллакс эффект
+ */
+function elementMove(element) {
+  var elementPosition = element.getBoundingClientRect();
+  element.style.left = elementPosition.top + 'px';
+}
+
+/**
+ * Экспортируем методы
+ */
 module.exports = {
-
-  /**
-   * Показываем элемент
-   */
-  showElement: function(element) {
-    element.classList.remove('invisible');
-  },
-
-  /**
-   * Скрываем элемент
-   */
-  hideElement: function(element) {
-    element.classList.add('invisible');
-  },
-
-  /**
-   * Метод Троттлинг (пропуск кадров)
-   */
-  throttle: function(callback, limit) {
-    var wait = false;
-    return function() {
-      if (!wait) {
-        callback.call();
-        wait = true;
-        setTimeout(function() {
-          wait = false;
-        }, limit);
-      }
-    };
-  },
-
-  /**
-   * Метод, который проверяет виден ли блок на экране
-   */
-  isElementVisible: function(target) {
-    var targetPosition = target.getBoundingClientRect();
-    return targetPosition.bottom > 0;
-  },
-
-  /**
-   * Смещение элемента на скролл сртаницы. Параллакс эффект
-   */
-  elementMove: function(element) {
-    var elementPosition = element.getBoundingClientRect();
-    element.style.left = elementPosition.top + 'px';
-  }
-
+  showElement: showElement,
+  hideElement: hideElement,
+  throttle: throttle,
+  isElementVisible: isElementVisible,
+  elementMove: elementMove
 };
